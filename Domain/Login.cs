3a984d9c8cbe27989db03cc814e594423861ab67
Balaec26 <balaec26@gmail.com>
2017-12-07 13:45:02 +0530

    b

---
+++ Domain/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Login
+    {
+        public int info_id { get; set; }
+       
+        public string Login_Time { get; set; }
+        public string Logout_Time { get; set; }
+        public string Logout_type { get; set; }
+        public string user_mac_address { get; set; }
+    }
+}
---
+++ Mapping/LoginProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using DataAccess;
+using AutoMapper;
+namespace Mapping
+{
+    class LoginProfile:Profile
+    {
+       
+       public LoginProfile()
+        {
+            CreateMap<Login, tbl_login_status>();
+           
+        }
+
+    }
+}
